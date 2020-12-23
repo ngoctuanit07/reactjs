a5e0120cfca8af3af0f8a21c4ac91bfcaee5f161
Walter <walternguyen07@gmail.com>
2020-12-23 15:53:52 +0700

    add data

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-function Greeting(){
-    return <h4>This is first component</h4>
+/*
+* JSX rules
+* return single element
+* div /section /article or Fragment
+* use cameCase for html attribute
+* className instead ofclass
+* close every element
+* formatting
+* */
+/*function Greeting(){
+    return <h4>Hello world</h4>
+}*/
+const Greeting = () => {
+    return React.createElement('div',{},React.createElement('h1',{},'hello world'));
 }
 ReactDOM.render(<Greeting/>,document.getElementById('root'));
